1d51f19dad450a5f02529af0e30dcb93cb4ee0a5
Akash Naskar <65643967+akashnaskar@users.noreply.github.com>
2022-11-01 08:42:51 +0530

    Add files via upload

---
+++ Ben_to_Eng_NMT/translator/__init__.py
@@ -0,0 +1,3 @@
+""" This has been coded by Akash Naskar
+
+for the final year research project""" 
---
+++ Ben_to_Eng_NMT/translator/config.py
@@ -0,0 +1,18 @@
+NUM_DATA_TO_LOAD = 3000
+
+EPOCHS = 100
+BATCH_SIZE = 64
+
+EMBEDDING_DIM = 256
+UNITS = 1024
+
+MAX_INPUT_LANG_LEN = 13
+MAX_TARGET_LANG_LEN = 10
+
+RESTORE_SAVED_CHECKPOINT = True
+
+""" This has been coded by Akash Naskar
+
+for the final year research project
+
+2021-2022, Christ Uni""" 
